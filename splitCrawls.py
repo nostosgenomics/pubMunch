e0f1a190f7e1525ff69c327c232d1c44bfd94b38
never <dnever@gmail.com>
2022-11-24 13:11:51 +0100

    fix folder structures

--- splitCrawls.py
+++ splitCrawls.py
@@ -7,7 +7,7 @@ import os
 nr_of_crawls = 10
 
 # read-in list of ids to crawl
-path_to_crawl_folder = './NewCrawl/'
+path_to_split_crawls = './SplitCrawl/'
 id_filename = 'pmids.txt'
 path_to_id = './RocioCrawl/' + id_filename
 id_list = pd.read_csv(path_to_id, header=None)
@@ -15,10 +15,11 @@ id_splits = np.linspace(0, len(id_list), nr_of_crawls).round()
 
 # create separate folders for each id split
 list_nr = 0
+os.mkdir(path_to_split_crawls)
 for i in id_splits[0:-1]:
     print('Processing split: ' + str(list_nr))
     new_list = id_list.loc[list_nr:id_splits[list_nr+1]]
-    path_to_folder = path_to_crawl_folder + 'crawl' + str(list_nr) + '/'
+    path_to_folder = path_to_split_crawls + 'crawl' + str(list_nr) + '/'
     os.mkdir(path_to_folder)
     new_filename = path_to_folder + id_filename
     new_list.to_csv(new_filename, index=None, header=None)
