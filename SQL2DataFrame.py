6bca0dfad8bd4f12ab233afa660611b7e3686cee
never <dnever@gmail.com>
2022-11-25 11:42:43 +0100

    df to file

--- SQL2DataFrame.py
+++ SQL2DataFrame.py
@@ -3,7 +3,10 @@ import sqlite3
 import pandas as pd
 
 # Connect to the database
-con = sqlite3.connect('./myCrawl/articles.db')
+path_to_crawl = './RocioCrawl/'
+db_name = 'articles.db'
+path_to_db = path_to_crawl + db_name
+con = sqlite3.connect(path_to_db)
 
 # Run SQL
 sql_query = pd.read_sql('SELECT * FROM articles', con)
@@ -11,3 +14,7 @@ sql_query = pd.read_sql('SELECT * FROM articles', con)
 # Convert SQL to DataFrame
 df = pd.DataFrame(sql_query)
 print(df)
+
+# save df to file
+df_name = path_to_crawl + db_name.split('.')[0]+'.csv'
+df.to_csv(df_name, encoding='utf-8')
